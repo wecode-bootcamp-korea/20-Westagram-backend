8d1355fa0e8216887c8cf38a991f290ede6b6d1a
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-01 18:32:40 +0900

    UPDATE: user/models.py

--- students/wooseok-choi/user/models.py
+++ students/wooseok-choi/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.CharField(max_length = 45)
+    password = models.CharField(max_length = 45)
+
+    class Meta:
+        db_table = 'users'
+
