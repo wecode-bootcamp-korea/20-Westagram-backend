99dd5e6968826c3c34d6380e1dd2d7c21aafd05c
Dae-Hwan <gigymi2005@naver.com>
2021-05-01 16:33:35 +0900

    회원가입 엔드포인트

---
+++ students/dae-hwan/user/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class User(models.Model):
+    email        = models.CharField(max_length=254)
+    password     = models.CharField(max_length=45) 
+    nick_name    = models.CharField(max_length=45, null = True)
+    phone_number = models.CharField(max_length=45, null=True)
+    create_at    = models.DateTimeField(auto_now_add=True)
+    update_at    = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.nick_name
---
+++ students/dae-hwan/user/views.py
@@ -0,0 +1,47 @@
+import json 
+
+from django.http  import JsonResponse
+from django.views import View
+
+from user.models  import User
+
+class SignUpView(View):
+    def post(self, request):
+        data         = json.loads(request.body)
+        email        = data['email']
+        password     = data['password']
+        nick_name    = data['nick_name']
+        phone_number = data['phone_number']
+
+        # 정보가 잘 입력되었을 때
+        try: 
+            # 이메일에 관한 에러
+            if '@' not in email or '.' not in email:
+                return JsonResponse({'message': 'email_validation'}, status = 400)
+            
+            # 비밀번호에 관한 에러
+            min_password = 8
+            if len(password) < min_password:
+                return JsonResponse({'message': 'password validation'}, status = 400)
+        
+            # 닉네임에 관한 에러
+            if User.objects.filter(nick_name = nick_name).exists():
+                return JsonResponse({'message': 'nick_name exist'}, status = 400)
+
+            # 전화번호에 관한 에러
+            if User.objects.filter(phone_number = phone_number).exists():
+                return JsonResponse({'message': 'phone_number exist'}, status = 400)
+
+            # email이 중복되지 않을시 데이터 생성
+            if not User.objects.filter(email = email).exists():
+                User.objects.create(
+                        email        = email,
+                        password     = password,
+                        nick_name    = nick_name,
+                        phone_number = phone_number,
+                )
+                return JsonResponse({'message': 'SUCCESS'}, status = 201)
+
+        # 정보를 틀리게 입력했을 때
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
