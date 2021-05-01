9eef3a6b4991a63d7bc4ff623c20357bbc764784
hwaya2828 <hwaya2828@gmail.com>
2021-05-01 14:13:22 +0900

    DONE: SIGNUP_1 (NEED TO MODIFY)

--- students/mihwa-yang/users/models.py
+++ students/mihwa-yang/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email    = models.EmailField(max_length=200)
+    password = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=45, null=True)
+    phone    = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'users'
--- students/mihwa-yang/users/views.py
+++ students/mihwa-yang/users/views.py
@@ -1,1 +1,36 @@
-# Create your views here.
+import json
+
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models import User
+
+
+class SignUpView(View):
+    
+    def post(self, request):
+        users = User.objects.all()
+        data  = json.loads(request.body)
+
+        if 'password' not in data.keys() or 'email' not in data.keys():
+            return JsonResponse({"MESSAGE": "KEY ERROR"}, status=400)
+
+        elif data['email'].count('@') == 0 or data['email'].count('.') == 0:
+            return JsonResponse({"MESSAGE": "EMAIL FORM ERROR"}, status=400)
+
+        elif len(data['password']) < 8:
+            return JsonResponse({"MESSAGE": "PASSWORD ERROR"}, status=400)
+
+        elif (User.objects.filter(email=data.get('email')).exists() == True) or ((User.objects.filter(nickname=data.get('nickname')).exists() == True) and (User.objects.filter(nickname=data.get('nickname') != None))) or ((User.objects.filter(phone=data.get('phone')).exists() == True) and (User.objects.filter(phone=data.get('phone') != None))):
+            return JsonResponse({"MESSAGE": "ALREADY EXIST ERROR"}, status=400)
+
+        else:
+            User.objects.create(
+                email    = data.get('email'),
+                password = data.get('password'),
+                nickname = data.get('nickname'),
+                phone    = data.get('phone')
+            )
+            return JsonResponse({'MESSAGE':'SIGNUP_SUCCESS'}, status=201)
+
+        
