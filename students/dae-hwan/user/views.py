40b26568358f0b04afe97eafc21c8bcfb96dc6dc
Dae-Hwan <gigymi2005@naver.com>
2021-05-02 17:22:11 +0900

    회원가입 엔드포인트(피드백 받고 오류수정)

--- students/dae-hwan/user/views.py
+++ students/dae-hwan/user/views.py
@@ -8,36 +8,37 @@ from user.models  import User
 
 class SignUpView(View):
     def post(self, request):
-        data                = json.loads(request.body)
-        email               = data['email']
-        password            = data['password']
-        nick_name           = data['nick_name']
-        phone_number        = data['phone_number']
-        email_validation    = re.compile( '^[a-z0-9]+@[a-z0-9]+\.[a-z0-9.]+$', re.I)
-        password_validation = re.compile('.{8,}')
-
         # 정보가 잘 입력되었을 때
         try: 
+            data                = json.loads(request.body)
+            email               = data['email']
+            password            = data['password']
+            nick_name           = data['nick_name']
+            phone_number        = data['phone_number']
+            email_validation    = re.compile( '^[a-z0-9]+@[a-z0-9]+\.[a-z0-9.]+$', re.I)
+            MIN_PASSWORD        = 8          
+            password_validation = re.compile('.{%d,}' % (MIN_PASSWORD))
+
             # 이메일에 관한 에러
-            if '@' not in email or '.' not in email:
-                return JsonResponse({'message': 'email_validation'}, status = 400)
+            if not email_validation.match(email):
+                return JsonResponse({'message': 'invalid email'}, status = 400)
             
             # 비밀번호에 관한 에러
-            min_password = 8
-            if len(password) < min_password:
-                return JsonResponse({'message': 'password validation'}, status = 400)
+            if not password_validation.match(password):
+                return JsonResponse({'message': 'invalid password'}, status = 400)
         
             # 닉네임에 관한 에러
             if User.objects.filter(nick_name = nick_name).exists():
-                return JsonResponse({'message': 'nick_name exist'}, status = 400)
+                return JsonResponse({'message': 'existing nick_name'}, status = 400)
 
             # 전화번호에 관한 에러
             if User.objects.filter(phone_number = phone_number).exists():
-                return JsonResponse({'message': 'phone_number exist'}, status = 400)
+                return JsonResponse({'message': 'existing phone_number'}, status = 400)
 
             # email이 중복되지 않을시 데이터 생성
             if User.objects.filter(email = email).exists():
-                return JsonResponse({'message': 'email exist'}, status = 400)
+                return JsonResponse({'message': 'existing email'}, status = 400)
+            
             User.objects.create(
                     email        = email,
                     password     = password,
@@ -49,3 +50,4 @@ class SignUpView(View):
         # 정보를 틀리게 입력했을 때
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
+
