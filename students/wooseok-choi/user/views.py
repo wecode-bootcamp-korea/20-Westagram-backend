57c35d91dc8f51991f365c7bb61cd9b481eb026e
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-03 13:28:54 +0900

    Progressing2: user/views.py

--- students/wooseok-choi/user/views.py
+++ students/wooseok-choi/user/views.py
@@ -6,6 +6,16 @@ from django.views import View
 
 from user.models import User
 
+def validate_email(email):
+    regex = re.compile('^[a-z0-9+-_.]+@[a-z0-9-]+\.[a-z0-9-.]+$', re.I)
+    match = regex.match(str(email))
+    return bool(match)
+
+def validate_password(password):
+    regex = re.compile('^[a-z0-9_-]{8,16}$', re.I)
+    match = regex.match(str(password))
+    return bool(match)
+
 class signupView(View):
     def post(self, request):
         data = json.loads(request.body)
