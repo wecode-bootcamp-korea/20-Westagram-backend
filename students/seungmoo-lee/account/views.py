97d5e6d3a649646577cb7ba542c8af74af4a0d94
이승무 <test850317@gmail.com>
2021-05-03 13:32:07 +0900

    CREATE: 로그인     - account/views.py

--- students/seungmoo-lee/account/views.py
+++ students/seungmoo-lee/account/views.py
@@ -4,6 +4,7 @@ from json.decoder import JSONDecodeError
 from django.http.response import JsonResponse
 from django.views         import View
 from django.db            import IntegrityError
+from django.db.models     import Q
 
 from .models                    import User
 from .validations               import UserValidation
@@ -47,3 +48,36 @@ class SignUpView(View):
             return JsonResponse({'message': 'UNKNOWN_ERROR'}, status=400)
 
         return JsonResponse({'message': 'SUCCESS'}, status=201)
+
+# 로그인
+class SignInView(View):
+    def post(self, request):
+        userValidation = UserValidation()
+
+        try:
+            data       = json.loads(request.body)
+            account_id = data.get('account_id')
+            password   = data.get('password')
+
+            if userValidation.check_required_fields(account_id, password):
+                raise KeyError()
+
+            user = User.objects.filter((
+                Q(email=account_id) |
+                Q(phone_number=account_id) |
+                Q(nickname=account_id)) &
+                Q(password=password))
+
+            if not user.exists():
+                raise ValidError('INVALID_USER')
+
+        except JSONDecodeError as e:
+            return JsonResponse({'message': 'EMPTY_ARGS_ERROR'}, status=400)
+        except KeyError as e:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        except ValidError as e:
+            return JsonResponse({'message': f'{e}'}, status=401)
+        except Exception as e:
+            return JsonResponse({'message': 'UNKNOWN_ERROR'}, status=400)
+
+        return JsonResponse({'message': 'SUCCESS'}, status=200)
