98db54a67747bf7a8d6264829644d137c24efff9
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-03 17:05:06 +0900

    ADD: User Data Model

--- students/junsikchoi/users/models.py
+++ students/junsikchoi/users/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.forms.models import model_to_dict
 
-# Create your models here.
+
+class User(models.Model):
+
+    email = models.EmailField(
+        "user email",
+        max_length=254,
+        unique=True,
+        blank=False,
+    )
+    password = models.CharField(
+        "user password",
+        max_length=50,
+        blank=False,
+    )
+
+    username = models.CharField(
+        "user nickname",
+        max_length=50,
+        unique=True,
+        blank=True,
+    )
+
+    phone_number = models.CharField(
+        "user phone number",
+        max_length=17,
+        unique=True,
+        blank=True,
+    )
+
+    class Meta:
+        app_label = "users"
+        db_table = "users"
+
+    def __str__(self):
+        return self.email
+
+    def to_dict(self):
+        model_dict = model_to_dict(self)
+        # Prevent User Password Leaking
+        model_dict.pop("password")
+        return model_dict
