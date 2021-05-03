17b53d343ada1bda9ade8fe5ab38a54c770122aa
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-03 13:28:54 +0900

    Refactoring: user/views.py

--- students/wooseok-choi/user/views.py
+++ students/wooseok-choi/user/views.py
@@ -1,7 +1,7 @@
 import json
 import re
 
-from django.http import JsonResponse
+from django.http  import JsonResponse
 from django.views import View
 
 from user.models import User
@@ -9,7 +9,6 @@ from user.models import User
 def validate_email(email):
     regex = re.compile('^[a-z0-9+-_.]+@[a-z0-9-]+\.[a-z0-9-.]+$', re.I)
     match = regex.match(str(email))
-    print(match)
     return bool(match)
 
 def validate_password(password):
@@ -17,28 +16,23 @@ def validate_password(password):
     match = regex.match(str(password))
     return bool(match)
 
-
 class UserView(View):
     def post(self, request):
         data = json.loads(request.body)
         
         try:
-            data['email']
-            data['password']
-            data['phone_number']
-            data['nickname']
             if not validate_email(data['email']):
                 return JsonResponse({'MESSAGE':'INVALID EMAIL'}, status=400)
-            elif not validate_password(data['password']):
-                return JsonResponse({'MESSAGE':'INVALID PASSWORD'}, status=400)
-        except:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400) 
+            if not validate_password(data['password']):
+                return JsonResponse({'MESSAGE':'INVALID PASSWORD'}, status=400) 
 
-        User.objects.create(
-                user_email = data['email'], 
-                user_password = data['password'],
-                phone_number = data['phone_number'],
-                nickname = data['nickname']
+            User.objects.create(
+                    user_email    = data['email'], 
+                    user_password = data['password'],
+                    phone_number  = data['phone_number'],
+                    nickname      = data['nickname']
                 )
+        except:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
 
         return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
