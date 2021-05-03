e82927f908831a3bbfef2b5c9b1985de94900465
Mac <mac@Macui-MacBook-Pro.local>
2021-05-03 19:38:55 +0900

    [20기 문성호][MISSION2] 회원가입 초안, 정규식 수정 예정

--- students/sungho-moon/user/models.py
+++ students/sungho-moon/user/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db              import models
 
 # Create your models here.
+
+class Signup(models.Model):
+    nick_name   =models.CharField(max_length=45, unique=True)
+    email       =models.CharField(max_length=45, unique=True)
+    password    =models.CharField(max_length=45)
+    phone_number=models.CharField(max_length=20, unique=True)
+
+    class Meta:
+          db_table = 'signup'
+
+
--- students/sungho-moon/user/views.py
+++ students/sungho-moon/user/views.py
@@ -1,1 +1,33 @@
+import json
+
+from django.http  import JsonResponse
+from django.views import View
+
+from user.models  import Signup
+
+class SignupView(View):
+
+    def post(self,request):
+        data    = json.loads(request.body)
+        
+        try:
+            if (Signup.objects.filter(nick_name=data['nick_name']).exists()) or (Signup.objects.filter(email=data['email']).exists()) or (Signup.objects.filter(phone_number = data['phone_number']).exists()):
+                return JsonResponse({'MESSAGE':'User_already_exist'}, status=401)
+        
+            elif ('@' not in data['email']) or ('.' not in data['email']):
+                return JsonResponse({'MESSAGE':'Email_Invalid'}, status=401)
+            
+            elif len(data['password']) < 8 :
+                return JsonResponse({'MESSAGE':'Password_Invalid'}, status=401)
+            
+            else: Signup.objects.create(
+                nick_name=data['nick_name'],
+                email=data['email'],
+                password=data['password'],
+                phone_number=data['phone_number']
+                )
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+        except KeyError: 
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
 
