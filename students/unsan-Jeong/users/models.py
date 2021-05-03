8c55975b6afeea71a6b501b35ae240d0e8a574b9
Action2theFuture <dnstks0204@google.com>
2021-05-03 18:51:41 +0900

    2차 수정

--- students/unsan-Jeong/users/models.py
+++ students/unsan-Jeong/users/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
 class Users(models.Model):
-    id           = models.BigAutoField(primary_key= True)
-    email        = models.EmailField(max_length=128, verbose_name="user e-mail")
-    name         = models.CharField(max_length =32, verbose_name="use name")
-    password     = models.CharField(max_length=128, verbose_name="password")
-    phone_number = models.CharField(max_length=32, verbose_name="phone number")
-    nickname     = models.CharField(max_length=32, verbose_name="nickname")
+    email        = models.EmailField(max_length=128)
+    name         = models.CharField(max_length =32)
+    password     = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=32)
+    nickname     = models.CharField(max_length=32)
     age          = models.IntegerField(null=True, blank=True)
     create_at    = models.DateTimeField(auto_now_add=True)
     update_at    = models.DateTimeField(auto_now =True)
