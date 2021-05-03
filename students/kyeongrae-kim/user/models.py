1f50cbafab4a2c71a882dc52e8748600bb7ed926
kyeongraekim <kyeongray@gmail.com>
2021-05-04 00:13:03 +0900

    회원가입 기능구현v1.6

--- students/kyeongrae-kim/user/models.py
+++ students/kyeongrae-kim/user/models.py
@@ -1,12 +1,12 @@
 from django.db  import models
 
 class User(models.Model):
-    mobile_num      = models.CharField(max_length=255)
-    email           = models.CharField(max_length=255)
-    username        = models.CharField(max_length=255)
-    password        = models.CharField(max_length=255)
-    created_at      = models.DateTimeField(auto_now_add=True)
-    modified_at     = models.DateField(auto_now=True)
+    mobile_num    = models.CharField(max_length=255)
+    email         = models.CharField(max_length=255)
+    username      = models.CharField(max_length=255)
+    password      = models.CharField(max_length=255)
+    create_at     = models.DateTimeField(auto_now_add=True)
+    update_at     = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
--- students/kyeongrae-kim/user/views.py
+++ students/kyeongrae-kim/user/views.py
@@ -7,28 +7,32 @@ import re
 
 class SignUpView(View):
     def post(self, request):
-        data = json.loads(request.body)
-        users_info = User.objects
-        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        data     = json.loads(request.body)
+        regex    = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        PASSWORD = 8
 
         try:
             if not (re.search(regex, data['email'])):                   
                 return JsonResponse({'MESSAGE':'EMAIL_KEY_ERROR'}, status=400)
-            if len(data['password']) < 8:                               
+            
+            if len(data['password']) < PASSWORD:                               
                 return JsonResponse({'MESSAGE':'PASSWORD_KEY_ERROR'}, status=400)
-            if users_info.filter(email=data['email']):                  
+            
+            if  User.objects.filter(email=data['email']):                  
                 return JsonResponse({'MESSAGE':'USER_ALREADY_EXISTS'}, status=400)
-            if users_info.filter(username=data['username']):           
+            
+            if  User.objects.filter(username=data['username']):           
                 return JsonResponse({'MESSAGE':'USER_ALREADY_EXISTS'}, status=400)
-            if users_info.filter(mobile_num=data['mobile_num']):       
+            
+            if  User.objects.filter(mobile_num=data['mobile_num']):       
                 return JsonResponse({'MESSAGE':'USER_ALREADY_EXISTS'}, status=400)   
+            
             user = User.objects.create(                            
                 email           = data['email'],
                 password        = data['password'],
                 mobile_num      = data['mobile_num'],
                 username        = data['username'],
             )
-
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
 
         except KeyError:
