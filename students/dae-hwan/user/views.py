ec1523a61bb354ec7058563854e8f26b8c091f90
Dae-Hwan <gigymi2005@naver.com>
2021-05-03 09:04:42 +0900

    로그인 엔드포인트

--- students/dae-hwan/user/views.py
+++ students/dae-hwan/user/views.py
@@ -61,9 +61,12 @@ class LogInView(View):
             data     = json.loads(request.body)
             email    = data['email']
             password = data['password']
-
+            
+            # 이메일이나 비밀번호가 일치하지 않을 때
             if not User.objects.filter(email = email, password = password).exists():
                 return JsonResponse({'message': 'invalid user'}, status = 401)
+            
+            return JsonResponse({'message': 'SUCCESS'}, status = 200)
 
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
