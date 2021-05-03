0048742f514b5ebb70faf22cdee7526be82d024e
김하민 <khmin1017@gmail.com>
2021-05-03 21:07:16 +0900

    westagram 회원가입 API 생성

--- students/hamin-Kim/user/models.py
+++ students/hamin-Kim/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    email       = models.CharField(max_length=45, unique=True)
+    password    = models.IntegerField()
+    nickname    = models.CharField(max_length=20, unique=True, blank=True)
+    phonenumber = models.IntegerField(unique=True, blank=True)
+    create_date = models.DateField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
