45339db6f4be83e8c73ad6098050432ec7c8e46d
Action2theFuture <dnstks0204@google.com>
2021-05-03 17:51:45 +0900

    1차 수정

--- students/unsan-Jeong/postings/views.py
+++ students/unsan-Jeong/postings/views.py
@@ -11,32 +11,34 @@ class PostingsView(View):
     def post(self, request):
         data = json.loads(request.body)
         users = Users.objects.all()
+        
         for user in users:
             if user.name == data['user_name']:
                 Postings.objects.create(
-                    name = data['name'],
-                    user_name=data['user_name'],
-                    image_url=data['url'],
-                    user=user
+                    name      = data['name'],
+                    user_name = data['user_name'],
+                    image_url = data['url'],
+                    user      = user
                 )
                 return JsonResponse({'MASSAGE':'SUCCESS'}, status = 201)
             return JsonResponse({'MASSAGE':'Non-existent users'}, status=400)
+    
     def get(self, request):
         postings = Postings.objects.values()
         return JsonResponse({'Postings':list(postings)}, status = 200)
 
 class CommentsView(View):
     def post(self, request):
-        data = json.loads(request.body)
-        users = Users.objects.all()
+        data     = json.loads(request.body)
+        users    = Users.objects.all()
         postings = Postings.objects.all()
         for user in users:
             for posting in postings:
                 if (user.name == data['user_name']) and (posting.name == data['posting_name']):
                     Comments.objects.create(
                         content = data['content'],
-                        user = user,
-                        post = posting,
+                        user    = user,
+                        post    = posting,
                     )
                     return JsonResponse({'MASSEGE':"SUCCESS"}, status =201)
                 else:
--- students/unsan-Jeong/users/views.py
+++ students/unsan-Jeong/users/views.py
@@ -10,24 +10,30 @@ from users.models import Users
 class SignUp(View):
     def post(self, request):
         data = json.loads(request.body)
+        password_length = 8
         try:
-            if len(data['password']) < 8:
+            if len(data['password']) < password_length:
                 return JsonResponse({'MESSAGE':'password of at least eight characters'}, status=404)
+            
             if (re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', data['email']) == None):
                 return JsonResponse({'MESSAGE':'Invalid email'}, status=400)
+            
             if Users.objects.filter(name=data['name']).exists():
                 return JsonResponse({'MESSAGE':'Name already exists'}, status=400)
+            
             if Users.objects.filter(phone_number=data['phone_number']).exists():
                 return JsonResponse({'MESSAGE':'Phone_number already exists'}, status=400)
+            
             if Users.objects.filter(email=data['email']).exists():
                 return JsonResponse({'MESSAGE':'email already exists'}, status=400)
+            
             Users.objects.create(
-                name=data['name'],
-                phone_number=data['phone_number'],
-                nickname=data['nickname'],
-                age=data['age'],
-                password=data['password'],
-                email=data['email'],
+                name         = data['name'],
+                phone_number = data['phone_number'],
+                nickname     = data['nickname'],
+                age          = data['age'],
+                password     = data['password'],
+                email        = data['email'],
                 )
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
         except KeyError:
@@ -41,6 +47,7 @@ class LogIn(View):
             for user in users:
                 if user.email != data['email']:
                     return JsonResponse({"message": "INVALID_USER"}, status=401)
+                
                 if user.password != data['password']:
                     return JsonResponse({"message": "INVALID_USER"}, status=401)
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
@@ -52,8 +59,8 @@ class LogIn(View):
         for user in users:
             result.append(
                 {
-                    'user_email':user.email,
-                    'name':user.name,
+                    'user_email': user.email,
+                    'name'      : user.name,
                 }
             )
         return JsonResponse({'users_results':result}, status=200)
