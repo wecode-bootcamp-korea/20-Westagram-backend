9bd0bc9e2705721385a387f4c105746cd230090d
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-04 15:55:39 +0900

    Update: loginView

--- students/wooseok-choi/user/views.py
+++ students/wooseok-choi/user/views.py
@@ -4,8 +4,7 @@ from django.http  import JsonResponse
 from django.views import View
 
 from user.models   import User
-from user.validate import *
-
+from user.validate import validate_email, validate_password
 
 class signupView(View):
     def post(self, request):
@@ -14,37 +13,37 @@ class signupView(View):
         try:
             if not validate_email(data['email']):
                 return JsonResponse({'message':'INVALID EMAIL'}, status=400)
+
             if not validate_password(data['password']):
-                return JsonResponse({'message':'INVALID PASSWORD'}, status=400) 
-            if not duplicate_email(data['email']):
+                return JsonResponse({'message':'INVALID PASSWORD'}, status=400)
+
+            if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'message':'DUPLICATE EMAIL'}, status=409)
 
             User.objects.create(
                     email         = data['email'], 
                     password      = data['password'],
-                    phone_number  = data['phone_number'],
-                    nickname      = data['nickname']
+                    phone_number  = data.get('phone_number'),
+                    nickname      = data.get('nickname')
                 )
             return JsonResponse({'message': 'SUCCESS'})
-        except:
+        except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
-
 
 class loginView(View):
     def post(self, request):
         data = json.loads(request.body)
 
-        try:
-            log_email    = data['email']
-            log_password = data['password']
-            email = User.objects.get(user_email = log_email)
-            
-            if email.user_password == log_password:
-                return JsonResponse({"message":"SUCCESS"}, status=200)
-            else:
-                return JsonResponse({"message":"INVALID_USER"}, status=401)
+        log_email    = data.get('email')
+        log_password = data.get('password')
 
-        except KeyError:
-            return JsonResponse({'message':'KEY_ERROR'}, status=400)
-        except:
-            return JsonResponse({'message':'INVALID_USER'}, status=401)
+        if not log_email or not log_password:
+            return JsonResponse({"message":"KEY_ERROR"}, status=400)
+        
+        if not User.objects.filter(email=log_email).exists():
+            return JsonResponse({"message":"INVALID_USER"}, status=401)
+
+        if not User.objects.get(email=log_email).password == log_password:
+            return JsonResponse({"message":"INVALID_PASSWORD"}, status=401)
+        
+        return JsonResponse({"message":"SUCCESS"}, status=200)
