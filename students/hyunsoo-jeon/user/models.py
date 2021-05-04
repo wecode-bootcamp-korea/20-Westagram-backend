68f44271f21e3caab85c674cc1af41a20b371210
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2021-05-04 15:41:52 +0900

    mission2 회원가입

--- students/hyunsoo-jeon/user/models.py
+++ students/hyunsoo-jeon/user/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db   import models
 
-# Create your models here.
+
+class User(models.Model):
+    email        = models.EmailField(max_length=50, unique=True) 
+    password     = models.CharField(max_length=100) 
+
+    class Meta():
+        db_table = 'users'
+
--- students/hyunsoo-jeon/user/views.py
+++ students/hyunsoo-jeon/user/views.py
@@ -1,3 +1,34 @@
+import json, re
+
+from django.views             import View
+from django.http              import JsonResponse
+from django.core.exceptions   import ValidationError
+
+from .models                  import User
 
 
-# Create your views here.
+class SignupView(View):
+    def post(self, request):
+        data      = json.loads(request.body)
+        email_reg = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        regex     = re.compile(email_reg)
+
+        try:
+            if not regex.match(data['email']):
+                return JsonResponse ({"message": "This email is not valid"}, status=400)
+
+            elif len(data['password']) < 8:
+                 return JsonResponse ({"message": "This password is too short"}, status=400)
+
+            elif User.objects.filter(email = data['email']).exists():
+                return JsonResponse ({"message": "This email already exists."}, status=400)
+
+            else:
+                User.objects.create(
+                        email    = data['email'],
+                        password = data['password']
+                    )
+                return JsonResponse ({"message": "SUCCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"message" : 'KEY_ERROR'}, status=400)
