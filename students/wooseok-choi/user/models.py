fd575ae353c00c966c05fd5e7c471cf8fe6e49a6
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-04 14:30:06 +0900

    Refactor: user/models.py for allowed null

--- students/wooseok-choi/user/models.py
+++ students/wooseok-choi/user/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-
 class User(models.Model):
     email        = models.CharField(max_length = 254)
-    password     = models.CharField(max_length = 40)
-    phone_number = models.CharField(max_length = 20)
-    nickname     = models.CharField(max_length = 40)
+    password     = models.CharField(max_length = 80)
+    phone_number = models.CharField(max_length = 30, null=True, blank=True)
+    nickname     = models.CharField(max_length = 60, null=True, blank=True)
 
     class Meta:
         db_table = 'users'
-
