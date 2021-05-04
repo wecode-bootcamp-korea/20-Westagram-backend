16cc2bf848d9ff63c1b99a6c669ea09230375196
Dae-Hwan <gigymi2005@naver.com>
2021-05-04 13:45:52 +0900

    회원가입기능(닉네임, 전화번호 넣는것을 선택으로 바꿈)

--- students/dae-hwan/user/views.py
+++ students/dae-hwan/user/views.py
@@ -12,8 +12,8 @@ class SignUpView(View):
             data                = json.loads(request.body)
             email               = data['email']
             password            = data['password']
-            nick_name           = data['nick_name']
-            phone_number        = data['phone_number']
+            nick_name           = data.get('nick_name')
+            phone_number        = data.get('phone_number')
             email_validation    = re.compile( '^[a-z0-9]+@[a-z0-9]+\.[a-z0-9.]+$', re.I)
             MIN_PASSWORD        = 8          
             password_validation = re.compile('.{%d,}' % (MIN_PASSWORD))
@@ -24,10 +24,10 @@ class SignUpView(View):
             if not password_validation.match(password):
                 return JsonResponse({'message': 'invalid password'}, status = 400)
         
-            if User.objects.filter(nick_name = nick_name).exists():
+            if nick_name != None and User.objects.filter(nick_name = nick_name).exists():
                 return JsonResponse({'message': 'existing nick_name'}, status = 400)
 
-            if User.objects.filter(phone_number = phone_number).exists():
+            if nick_name != None and User.objects.filter(phone_number = phone_number).exists():
                 return JsonResponse({'message': 'existing phone_number'}, status = 400)
 
             if User.objects.filter(email = email).exists():
@@ -36,8 +36,8 @@ class SignUpView(View):
             User.objects.create(
                     email        = email,
                     password     = password,
-                    nick_name    = nick_name,
-                    phone_number = phone_number,
+                    nick_name    = data.get('nick_name'),
+                    phone_number = data.get('phone_number'),
             )
             return JsonResponse({'message': 'SUCCESS'}, status = 201)
 
