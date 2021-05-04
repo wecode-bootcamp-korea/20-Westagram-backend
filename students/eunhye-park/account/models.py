c16278505e505e1cc1c1c0ae92c674d4f094ec2e
박은혜 <ehye0922@gmail.com>
2021-05-04 20:50:45 +0900

    complete models.py

--- students/eunhye-park/account/models.py
+++ students/eunhye-park/account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class User(models.Model):
+    username     = models.CharField(max_lenght=45)
+    password     = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=45)
+    name         = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=45)
 
-# Create your models here.
+    class Meta:
+        db_table: "users"
