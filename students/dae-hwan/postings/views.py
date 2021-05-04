e04d350fe56d87fb7188aaddc03e9cd423b04839
Dae-Hwan <gigymi2005@naver.com>
2021-05-04 11:51:11 +0900

    게시물에 댓글 등록 기능 만듬

--- students/dae-hwan/postings/views.py
+++ students/dae-hwan/postings/views.py
@@ -3,7 +3,7 @@ import json
 from django.http     import JsonResponse
 from django.views    import View
 
-from postings.models import Posting
+from postings.models import Posting, Comment
 from user.models     import User
 
 class PostingView(View):
@@ -39,3 +39,29 @@ class PostingView(View):
                 }
             results.append(posting_information)
         return JsonResponse({'results': results}, status = 200)
+
+class CommentView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            content  = data['content']
+            email    = data['email']
+            comments = data['comments']
+
+            if not User.objects.filter(email = email).exists():
+                return JsonResponse({'message': 'invalid user'}, status = 401)
+
+            if not Posting.objects.filter(content = content).exists():
+                return JsonResponse({'message': 'invalid posting'}, status = 401)
+
+            Comment.objects.create(
+                    posting  = Posting.objects.get(content = content),
+                    user     = User.objects.get(email = email),
+                    comments = comments,
+            )
+            return JsonResponse({'message': 'SUCCESS'}, status =200)
+        
+        except KeyError:
+            JsonResponse({'message': 'KEY_ERROR'}, status = 400)
+                    
+            
