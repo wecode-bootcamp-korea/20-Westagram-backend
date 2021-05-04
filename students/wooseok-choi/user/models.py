ea0dc765e4cc9d8bf0705b45b8101e20a13aeec5
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-04 15:55:00 +0900

    Update: user/models allowed null at nickname, phone_number

--- students/wooseok-choi/user/models.py
+++ students/wooseok-choi/user/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-
 class User(models.Model):
-    email         = models.CharField(max_length = 45)
-    password      = models.CharField(max_length = 20)
-    phone_number  = models.CharField(max_length = 15)
-    nickname      = models.CharField(max_length = 40)
+    email         = models.CharField(max_length = 100)
+    password      = models.CharField(max_length = 50)
+    phone_number  = models.CharField(max_length = 30, null=True)
+    nickname      = models.CharField(max_length = 100, null=True)
 
     class Meta:
         db_table = 'users'
