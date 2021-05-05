f96400020c1ef3aa41aace31f5b342ccd3b0910e
Mac <mac@Macui-MacBook-Pro.local>
2021-05-05 14:00:24 +0900

    [20기 문성호][MISSION2]회원가입 END POINT: pr 피드백 사항 수정

--- students/sungho-moon/user/models.py
+++ students/sungho-moon/user/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db              import models
 
 # Create your models here.
+
+class USER(models.Model):
+    nick_name   =models.CharField(max_length=45, unique=True, null=True)
+    email       =models.CharField(max_length=100, unique=True)
+    password    =models.CharField(max_length=200)
+    phone_number=models.CharField(max_length=20, null=True)
+
+    class Meta:
+          db_table = 'users'
