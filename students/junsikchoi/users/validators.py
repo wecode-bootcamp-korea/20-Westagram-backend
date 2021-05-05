9a174fefd1bc44055f280f07cc55cd5b7715929b
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-05 22:16:00 +0900

    MODIFY: Simplify email validator logic

--- students/junsikchoi/users/validators.py
+++ students/junsikchoi/users/validators.py
@@ -2,14 +2,12 @@ from django.core.exceptions import ValidationError
 
 
 def validate_email(email):
-    required_character = ["@", "."]
 
-    for chracter in required_character:
-        if chracter not in email:
-            raise ValidationError(
-                f"{email} is not an valid email.",
-                params={"value": email},
-            )
+    if not ("@" in email and "." in email):
+        raise ValidationError(
+            f"{email} is not an valid email.",
+            params={"value": email},
+        )
 
 
 def validate_password(password):
