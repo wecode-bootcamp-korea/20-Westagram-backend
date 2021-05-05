0eff92aeabef184dff7edc6d37dc6e3c29e5ec29
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-05 20:59:06 +0900

    Add: encoding password in user/views.py

--- students/wooseok-choi/user/models.py
+++ students/wooseok-choi/user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     email         = models.CharField(max_length = 100)
-    password      = models.CharField(max_length = 50)
+    password      = models.BinaryField()
     phone_number  = models.CharField(max_length = 30, null=True)
     nickname      = models.CharField(max_length = 100, null=True)
 
--- students/wooseok-choi/user/views.py
+++ students/wooseok-choi/user/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http  import JsonResponse
 from django.views import View
+import bcrypt
 
 from user.models import User
 from user.validate import validate_email, validate_password
@@ -20,9 +21,11 @@ class SignupView(View):
             if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'message':'DUPLICATE EMAIL'}, status=409)
 
+            hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+            
             User.objects.create(
                     email         = data['email'], 
-                    password      = data['password'],
+                    password      = hashed_password,
                     phone_number  = data.get('phone_number'),
                     nickname      = data.get('nickname')
                 )
@@ -43,7 +46,9 @@ class LoginView(View):
         if not User.objects.filter(email=log_email).exists():
             return JsonResponse({"message":"INVALID_USER"}, status=401)
 
-        if not User.objects.get(email=log_email).password == log_password:
+        hashed_password = User.objects.get(email=log_email).password
+
+        if not bcrypt.checkpw(log_password.encode('utf-8'),hashed_password):
             return JsonResponse({"message":"INVALID_USER"}, status=401)
         
         return JsonResponse({"message":"SUCCESS"}, status=200)
