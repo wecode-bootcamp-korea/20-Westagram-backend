e608f34b20c642fa3fc0aefffc9032fcb1941a92
박은혜 <ehye0922@gmail.com>
2021-05-06 01:31:08 +0900

    complete signup

--- students/eunhye-park/account/models.py
+++ students/eunhye-park/account/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 class User(models.Model):
-    username     = models.CharField(max_lenght=45)
-    password     = models.CharField(max_length=45)
-    email        = models.EmailField(max_length=45)
-    name         = models.CharField(max_length=45)
-    phone_number = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=80)
+    password     = models.CharField(max_length=80)
+    phone_number = models.CharField(max_length=80)
+    username     = models.CharField(max_lenght=80)
 
     class Meta:
         db_table: "users"
--- students/eunhye-park/account/views.py
+++ students/eunhye-park/account/views.py
@@ -1,1 +1,41 @@
+import json
 
+from django.views   import View
+from django.http    import JsonResponse
+from account.models import User
+class SignupViews(View):
+    def post(self, request):
+            data = json.loads(request.body)
+    try:
+
+            if email == '' or password == '':
+                return JsonResponse({'MESSAGE':'KEY_ERROR'}, status = 400)
+
+            if '@' not in data['email'] or '.' not in data['email']:
+                return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status = 400) 
+
+            if User.objects.filter(phone_number=data.get['phone_number']).exist() \
+                 and User.objects.filter(nickname=data.get['phone_number']) != None):
+                return JsonResponse({'MESSAGE': 'ALREADY_EXISTS'}, status = 400)
+
+            if User.objects.filter(nickname=data.get['nickname']).exist() \
+                 and User.objects.filter(nickname=data.get['nickname']) != None):
+                return JsonResponse({'MESSAGE' : 'ALREADY_EXISTS'}, status = 400)
+
+            if User.objects.filter(email=data['email']).exist():
+                return JsonResponse({"MESSAGE" : 'ALREADY_EXISTS'}, status = 400)
+
+            PASSWORD_LENGTH = 8
+            if len(data['password']) < PASSWORD_LENGTH:
+                return JsonResponse({'MESSAGE' : 'INVALID PASSWORD'}, status = 400)
+    
+            Signup.objects.create(
+                email         = email,
+                password      = password,
+                nickname      = nickname,
+                phone_number  = phone_number
+            )
+            return JsonResponse({'Message' : 'SUCCESS'}, status = 201)
+   
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEYERROR'}, status=400)
