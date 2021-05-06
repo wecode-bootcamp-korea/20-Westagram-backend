0c72cbd20cdd4791f8cc25c197f06d2f853cd15c
Mac <mac@Macui-MacBook-Pro.local>
2021-05-06 09:56:12 +0900

    [20기 문성호][MISSION2]회원가입 endpoing 2차수정

--- students/sungho-moon/user/models.py
+++ students/sungho-moon/user/models.py
@@ -2,7 +2,7 @@ from django.db              import models
 
 # Create your models here.
 
-class USER(models.Model):
+class User(models.Model):
     nick_name   =models.CharField(max_length=45, unique=True, null=True)
     email       =models.CharField(max_length=100, unique=True)
     password    =models.CharField(max_length=200)
--- students/sungho-moon/user/views.py
+++ students/sungho-moon/user/views.py
@@ -3,7 +3,7 @@ import json
 from django.http  import JsonResponse
 from django.views import View
 
-from user.models  import USER
+from user.models  import User
 
 class UserView(View):
 
@@ -11,31 +11,31 @@ class UserView(View):
         data    = json.loads(request.body)
         PW_MIN_LENGTH = 8
 
-        data_email = data['email']
-        data_password = data['password']
-        data_nickname = data.get('nickname')
+        data_email       = data['email']
+        data_password    = data['password']
+        data_nickname    = data.get('nickname')
         data_phonenumber = data.get('phonenumber')
 
         try:
-            if (USER.objects.filter(email=data_email).exists()):
+            if (User.objects.filter(email=data_email).exists()):
                 return JsonResponse({'MESSAGE':'User_already_exist'}, status=401)
 
             if ('@' not in data['email']) or ('.' not in data['email']):
                 return JsonResponse({'MESSAGE':'Email_Invalid'}, status=401)
 
-            if (data_nickname != None) and (USER.objects.filter(nick_name=data_nickname).exists()): 
+            if (data_nickname != None) and (User.objects.filter(nick_name=data_nickname).exists()): 
                 return JsonResponse({'MESSAGE':'User_already_exist'}, status=401)
 
-            if (data_phonenumber != None) and (USER.objects.filter(phone_number = data['phone_number']).exists()):
+            if (data_phonenumber != None) and (User.objects.filter(phone_number = data['phone_number']).exists()):
                 return JsonResponse({'MESSAGE':'User_already_exist'}, status=401)
             
             if len(data['password']) < PW_MIN_LENGTH :
                 return JsonResponse({'MESSAGE':'Password_Invalid'}, status=401)
             
-            USER.objects.create(
-                nick_name=data_nickname,
-                email=data_email,
-                password=data_password,
+            User.objects.create(
+                nick_name   =data_nickname,
+                email       =data_email,
+                password    =data_password,
                 phone_number=data_phonenumber
                 )
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
