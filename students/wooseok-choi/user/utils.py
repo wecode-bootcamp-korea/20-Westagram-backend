400f05d78275a2e61a067d5d1afda29e6492c30a
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-06 17:48:47 +0900

    Add: user/utils.py about permission

---
+++ students/wooseok-choi/user/utils.py
@@ -0,0 +1,27 @@
+import jwt
+import json
+import requests
+
+from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+from westagram.settings import SECRET_KEY, ALGORITHM
+from user.models import User
+
+def log_permission(func):
+    def wrapper(self, request, *args, **kwargs):
+        token = request.headers.get('Authorization', None)
+        try:
+            payload      = jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
+            user         = User.objects.get(id = payload['id'])
+            request.user = user
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message' : 'INVALID_TOKEN' }, status=400)
+
+        except Account.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_USER'}, status=400)
+        
+        return func(self, request, *args, **kwargs)
+        
+    return wrapper
