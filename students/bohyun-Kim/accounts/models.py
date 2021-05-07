e629932b01ab9d3fee4377c9a6a5295bd3b1c16a
bohyunk001 <bohyunk001@gmail.com>
2021-05-07 13:03:17 +0900

    장고 회원가입 기능 추가

--- students/bohyun-Kim/accounts/models.py
+++ students/bohyun-Kim/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    phone_number = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    password = models.BinaryField(max_length = 500)
+
+    class Meta:
+        db_table = 'users'
--- students/bohyun-Kim/accounts/views.py
+++ students/bohyun-Kim/accounts/views.py
@@ -1,3 +1,51 @@
 
 
 # Create your views here.
+import json
+import re
+
+from django.http     import JsonResponse
+from django.views    import View
+from accounts.models import User
+
+class AccountView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        data = json.loads(request.body)
+
+        email = data["email"]
+        password = data["password"]
+        phone_number = data["phone_number"]
+        user_name = data["user_name"]
+        
+        if not ('email' in data.keys() or 'password' in data.keys()):
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+        if not ('@' in email and '.' in email):
+            return JsonResponse({'MESSAGE':'INVALID EMAIL'}, status=400)
+        if len(password) < 8:
+            return JsonResponse({'MESSAGE':'INVALID PASSWORD'}, status=400)
+        
+        if (
+            User.objects.filter(email=email).exists()
+            or User.objects.filter(user_name=user_name).exists()
+            or User.objects.filter(phone_number=phone_number).exists()
+        ):
+            return JsonResponse({'MESSAGE':'EXISTING USER DATA'}, status=400)
+
+        
+        User.objects.create(
+            email=data['email'],
+            phone_number=data['phone_number'],
+            user_name=data['user_name'],
+            password=data['password'],
+        )
+        
+
+        return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
+
+class AuthView(View):
+    def post(self, request):
+        pass
